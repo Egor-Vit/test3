ac3642bdaecf74f7c120afc16f4f430be93e474a
Vit-Egor <vit.egor2016@yandex.ru>
2022-03-04 14:21:09 +0500

    new plus

--- test3/Program.cs
+++ test3/Program.cs
@@ -10,6 +10,22 @@ namespace test3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите А");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите А");
+            Console.WriteLine("Решение:\n+Сложение, \n-Вычетание, \n*Умножение, \n/Деление");
+            string X = Console.ReadLine();
+            string plus;
+            string minus;
+            string um;
+            string del;
+            if (X == "+")
+            {
+                int C = A + B;
+                Console.WriteLine("Ответ:" + C);
+            }
         }
     }
 }
